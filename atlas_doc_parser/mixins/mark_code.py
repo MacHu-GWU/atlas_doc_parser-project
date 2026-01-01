5137e5ffc49b887885b71b4929b8a0e65b7ca151
Sanhe Hu <6800411+MacHu-GWU@users.noreply.github.com>
2026-01-01 10:58:28 -0500

    Create mark_code.py

---
+++ atlas_doc_parser/mixins/mark_code.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+"""
+Mixin for ADF Mark: code
+
+This file is NOT auto-generated. Implement to_markdown() method here.
+"""
+
+import typing as T
+
+if T.TYPE_CHECKING:  # pragma: no cover
+    from ..nodes.mark_code import MarkCode
+
+
+class MarkCodeMixin:
+    def to_markdown(self: "MarkCode", text: str) -> str:
+        return f"`{text}`"
