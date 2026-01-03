19b60bb2871c81110a5b41e969d7e46ed80a55b5
Sanhe Hu <6800411+MacHu-GWU@users.noreply.github.com>
2026-01-02 21:52:45 -0500

    Add test and sample for NodeTaskList
    Introduces a JSON sample for a nested task list structure and a basic test case for NodeTaskList using this sample. This helps ensure correct parsing and handling of task lists in the document parser.

---
+++ tests/nodes/test_nodes_node_task_list.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+from atlas_doc_parser.nodes.node_task_list import NodeTaskList
+
+from atlas_doc_parser.tests.data.samples import AdfSampleEnum
+
+
+class TestNodeBulletList:
+    def test_task_list_node_basic(self):
+        node = AdfSampleEnum.node_task_list.test(NodeTaskList)
+
+
+if __name__ == "__main__":
+    from atlas_doc_parser.tests import run_cov_test
+
+    run_cov_test(
+        __file__,
+        "atlas_doc_parser.nodes.node_task_list",
+        preview=False,
+    )
